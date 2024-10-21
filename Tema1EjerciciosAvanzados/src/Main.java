df2993d31799321bf0590735735e374d96a8a4bc
Antonio Contreras Cardenas <aconcar181@g.educaand.es>
2024-10-21 20:45:26 +0200

    Cambios

--- Tema1EjerciciosAvanzados/src/Main.java
+++ Tema1EjerciciosAvanzados/src/Main.java
@@ -1,5 +1,5 @@
-import EjListaHilos.classes.ProductoLista;
-import EjListaHilos.controllers.FactoryList;
+import classes.Producto;
+import controllers.Factory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,16 +9,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<ProductoLista> listaProductos = new ArrayList<ProductoLista>();
-        listaProductos.add(new ProductoLista("Producto 1"));
-        listaProductos.add(new ProductoLista("Producto 2"));
-        listaProductos.add(new ProductoLista("Producto 3"));
-        listaProductos.add(new ProductoLista("Producto 4"));
-        listaProductos.add(new ProductoLista("Producto 5"));
-        listaProductos.add(new ProductoLista("Producto 6"));
+        List<Producto> listaProductos = new ArrayList<Producto>();
+        listaProductos.add(new Producto("Producto 1"));
+        listaProductos.add(new Producto("Producto 2"));
+        listaProductos.add(new Producto("Producto 3"));
+        listaProductos.add(new Producto("Producto 4"));
+        listaProductos.add(new Producto("Producto 5"));
+        listaProductos.add(new Producto("Producto 6"));
 
 
-        FactoryList factory = new FactoryList(listaProductos);
+        Factory factory = new Factory(listaProductos);
         factory.startWorkers();
 
 
